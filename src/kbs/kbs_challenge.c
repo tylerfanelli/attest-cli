6af22ff825c84f2b73c1ccb46a25c81e835bd7fd
Tyler Fanelli <tfanelli@redhat.com>
2022-11-16 00:48:26 -0500

    kbs/challenge: Cleanup error handling
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- src/kbs/kbs_challenge.c
+++ src/kbs/kbs_challenge.c
@@ -15,29 +15,28 @@ kbs_challenge_parse(char *str, kbs_challenge_t *challenge)
                 return 0;
 
         nonce = malloc(BUF_MAX);
-        if (!nonce) {
-                free(eparams);
-                return 0;
-        }
+        if (!nonce)
+                goto err_eparams;
 
         ret = json_parse_label(str, "extra-params", &eparams);
-        if (!ret) {
-                free(eparams);
-                free(nonce);
-
-                return 0;
-        }
+        if (!ret)
+                goto err;
 
         ret = json_parse_label(str, "nonce", &eparams);
-        if (!ret) {
-                free(eparams);
-                free(nonce);
-
-                return 0;
-        }
+        if (!ret)
+                goto err;
 
         challenge->extra_params = (void *) eparams;
         challenge->nonce = nonce;
 
         return 1;
+
+
+err:
+        free(nonce);
+
+err_eparams:
+        free(eparams);
+
+        return 0;
 }
